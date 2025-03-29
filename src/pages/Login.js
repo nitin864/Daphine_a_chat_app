329d5f0291e771d3496cb3d7dd86cc006fc32d9b
NITIN <91000126+nitin864@users.noreply.github.com>
2025-03-29 09:25:42 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Daphin by SIBU gand</span>
+        <span className="logo">Daphin by Nitin</span>
         <span className="title">Login</span>
         <form onSubmit={handleSubmit}>
           <input type="email" placeholder="email" />
